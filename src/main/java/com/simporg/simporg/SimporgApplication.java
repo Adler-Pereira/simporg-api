9cd55787d8483002f21a42ee978ce834b3101d5d
Adler-Pereira <adlerlimap@gmail.com>
2025-09-08 00:22:35 -0300

    feat: create simporg project

---
+++ src/main/java/com/simporg/simporg/SimporgApplication.java
@@ -0,0 +1,13 @@
+package com.simporg.simporg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimporgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimporgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/simporg/simporg/SimporgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simporg.simporg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimporgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
